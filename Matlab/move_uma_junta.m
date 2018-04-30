2602b9710c76eecca212a0140e52dca000ac49e6
Pedro Matheus <pedro.matheusfs@gmail.com>
2018-04-29 22:47:52 -0300

    code to move a single joint

--- Matlab/move_uma_junta.m
+++ Matlab/move_uma_junta.m
@@ -6,8 +6,8 @@ vrep=remApi('remoteApi');
 vrep.simxFinish(-1);
 clientID=vrep.simxStart('127.0.0.1',19997,true,true,5000,5);
 clientID
-pos = [0 30 45 60 90];
-pos_rad = (pos*pi)/180; 
+pos = [0 -60];
+pos_rad = ((pos*pi)/180)
 %pos_rad = pos
 k=1;
 if (clientID>-1)
@@ -16,21 +16,15 @@ if (clientID>-1)
     %vrep.simxStartSimulation(clientID,vrep.simx_opmode_blocking);
     %Handles
     
-    [returnCode, junta5]=vrep.simxGetObjectHandle(clientID,'Jmotor5',vrep.simx_opmode_blocking);
+    [returnCode, junta5]=vrep.simxGetObjectHandle(clientID,'Jmotor3',vrep.simx_opmode_blocking);
     %if(returnCode==0)
     %disp(strcat('Jmotor',int2str(i),' Connected')) 
      [~,present_pos]=vrep.simxGetJointPosition(clientID,junta5,vrep.simx_opmode_streaming)
      %present_pos = 0;
      %while(true)
-      [~]=vrep.simxSetJointTargetPosition(clientID,junta5,pos_rad(1),vrep.simx_opmode_streaming);
+      [~]=vrep.simxSetJointTargetPosition(clientID,junta5,pos_rad(1),vrep.simx_opmode_oneshot);
       pause(2)
-      [~]=vrep.simxSetJointTargetPosition(clientID,junta5,pos_rad(2),vrep.simx_opmode_streaming);
-      pause(2)
-      [~]=vrep.simxSetJointTargetPosition(clientID,junta5,pos_rad(3),vrep.simx_opmode_streaming);
-      pause(2)
-      [~]=vrep.simxSetJointTargetPosition(clientID,junta5,pos_rad(4),vrep.simx_opmode_streaming);
-      pause(2)
-      [~]=vrep.simxSetJointTargetPosition(clientID,junta5,pos_rad(5),vrep.simx_opmode_streaming);
+      [~]=vrep.simxSetJointTargetPosition(clientID,junta5,pos_rad(2),vrep.simx_opmode_oneshot);
       pause(2)
       [~,present_pos]=vrep.simxGetJointPosition(clientID,junta5,vrep.simx_opmode_buffer) 
      %[~]=vrep.simxSetJointTargetVelocity(clientID,junta6,0.1,vrep.simx_opmode_blocking);
