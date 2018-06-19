480d9a5acacbd71834d01b21f6e70616d9d914ac
Vitor Duarte <vitor.alves.duarte@gmail.com>
2018-06-19 15:39:49 -0300

    Plot acceleration

--- Matlab/gait_analysis.m
+++ Matlab/gait_analysis.m
@@ -20,37 +20,44 @@ function gait_analysis(folder_num)
     y_acc = sensor_data(:,3);
     z_acc = sensor_data(:,4);
 
+    % Filter acceleration
+    lp_filter = designfilt('lowpassfir', 'PassbandFrequency', 75, 'StopbandFrequency', 180, 'PassbandRipple', 1, 'StopbandAttenuation', 60, 'SampleRate', 500);
+    
+    x_acc_filter = filter(lp_filter, x_acc);
+    y_acc_filter = filter(lp_filter, y_acc);
+    z_acc_filter = filter(lp_filter, z_acc);
+    
+    delay = round(mean(grpdelay(lp_filter)));
+    [~, x_acc_filt_fix] = fix_delay(timestamp, x_acc_filter, delay);
+    [~, y_acc_filt_fix] = fix_delay(timestamp, y_acc_filter, delay);
+    [timestamp_filt_fix, z_acc_filt_fix] = fix_delay(timestamp, z_acc_filter, delay);
+    
     % Get roll and pitch
     roll = rad2deg(atan2(y_acc, z_acc));
     pitch = rad2deg(atan2((-x_acc), sqrt(power(y_acc, 2) + power(z_acc, 2))));
-
-    % Filter roll and pitch
-    lp_filter = designfilt('lowpassfir', 'PassbandFrequency', 75, 'StopbandFrequency', 180, 'PassbandRipple', 1, 'StopbandAttenuation', 60, 'SampleRate', 500);
-    roll_filtered = filter(lp_filter,roll);
-    pitch_filtered = filter(lp_filter, pitch);
-
-    % Fix time delay of roll and pitch
-    delay = round(mean(grpdelay(lp_filter)));
-    [~, roll_filt_fix] = fix_delay(timestamp, roll_filtered, delay);
-    %roll = roll(1:end-delay);
-    [timestamp_filt_fix, pitch_filt_fix] = fix_delay(timestamp, pitch_filtered, delay);
-    %pitch = pitch(1:end-delay);
-
+    
+    %Fix to fit on new timestamp
+    roll = roll(1:end-delay);
+    pitch = pitch(1:end-delay);
+    
+    roll_filt_fix = rad2deg(atan2(y_acc_filt_fix, z_acc_filt_fix)); 
+    pitch_filt_fix = rad2deg(atan2((-x_acc_filt_fix), sqrt(power(y_acc_filt_fix, 2) + power(z_acc_filt_fix, 2))));
+    
     % Plot roll and pitch
     figure(1)
     subplot(2,1,1);
     plot(timestamp_filt_fix, roll_filt_fix);
     grid on;
-    %hold on
-    %plot(timestamp_filt_fix, roll);
+    hold on
+    plot(timestamp_filt_fix, roll);
     title({'Gráfico do valor estimado do ângulo de rolagem'});
     xlabel('Tempo (s)');
     ylabel('Ângulo (graus)');
     subplot(2,1,2);
     plot(timestamp_filt_fix, pitch_filt_fix);
     grid on;
-    %hold on
-    %plot(timestamp_filt_fix, pitch);
+    hold on
+    plot(timestamp_filt_fix, pitch);
     title({'Gráfico do valor estimado do ângulo de arfagem'});
     xlabel('Tempo (s)');
     ylabel('Ângulo (graus)');
@@ -64,4 +71,22 @@ function gait_analysis(folder_num)
         xlabel('Tempo(s)');
         ylabel('Posição(pwm)'); 
     end
+    
+    % Plot acceleration
+    figure(3)
+    subplot(3,1,1);
+    plot(timestamp_filt_fix, x_acc_filt_fix),
+    title('Aceleração em x vs Tempo');
+    xlabel('Tempo(s)');
+    ylabel('Aceleração (m/s²)'); 
+     subplot(3,1,2);
+    plot(timestamp_filt_fix, y_acc_filt_fix),
+    title('Aceleração em y vs Tempo');
+    xlabel('Tempo(s)');
+    ylabel('Aceleração (m/s²)'); 
+     subplot(3,1,3);
+    plot(timestamp_filt_fix, z_acc_filt_fix),
+    title('Aceleração em z vs Tempo');
+    xlabel('Tempo(s)');
+    ylabel('Aceleração (m/s²)'); 
     
